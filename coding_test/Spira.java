9e7dcf54187ff8ba46c8743c60358efe67da801f
Pionia5375 <dlwjd8023@gmail.com>
2026-02-19 22:39:13 +0900

    파일 저장 반영

--- coding_test/Spira.java
+++ coding_test/Spira.java
@@ -1,11 +1,11 @@
 public class Spira {
     public static void main(String[] args) {
         spira(5);
-    } 
+    }
 
     static void spira(int n) {
         int len = 1 + (2 * (n - 1));
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < len; j++) {
                 if (j < len / 2 - i || j > len / 2 + i)
                     System.out.print(" ");
@@ -15,6 +15,4 @@ public class Spira {
             System.out.println();
         }
     }
-
-    
 }
