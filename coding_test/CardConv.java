1dd13095929a24eea5af3fd493c6b103bbae3548
Pionia5375 <dlwjd8023@gmail.com>
2026-02-20 18:09:04 +0900

    20260220 공부내용 중간저장

---
+++ coding_test/CardConv.java
@@ -0,0 +1,56 @@
+package me.leejeong;
+
+import java.util.Scanner;
+
+public class CardConv {
+
+    static int cardConv(int x, int r, char[] d) {
+        int digits = 0;
+        String dchar = "01234566789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        do {
+            d[digits++] = dchar.charAt(x % r);
+            x /= r;
+        } while (x != 0);
+
+        for (int i = 0; i < digits / 2; i++) {
+            char t = d[i];
+            d[i] = d[digits - i - 1];
+            d[digits - i - 1] = t;
+        }
+
+        return digits;
+    }
+
+    static void main() {
+        Scanner stdIn = new Scanner(System.in);
+        int no;
+        int cd;
+        int dno;
+        int retry;
+        char[] cno = new char[32]; // 최대 길이: 32
+
+        System.out.println("10진수를 기수변환합니다.");;
+        do {
+            do {
+                System.out.print("변환할 음이 아닌 10진수 정수: ");
+                no = stdIn.nextInt();
+            } while (no < 0);
+
+            do {
+                System.out.print("어쩐 진수로 변활까요? (2~36): ");
+                cd = stdIn.nextInt();
+            } while (cd < 2 || cd > 36);
+
+            dno = cardConv(no, cd, cno);
+
+            System.out.print(cd + "진수로 ");
+            for (int i = 0; i < dno; i++)
+                System.out.print(cno[i]);
+            System.out.println("입니다.");
+
+            System.out.print("한 번 더 할까요? (1. 예 / 0. 아니요): ");
+            retry = stdIn.nextInt();
+        } while (retry == 1);
+    }
+}
---
+++ coding_test/ReverseArray.java
@@ -0,0 +1,36 @@
+package me.leejeong;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+    }
+
+    static void reverse(int[] a) {
+        for (int i = 0; i < a.length / 2; i++) {
+            swap(a, i, a.length - i - 1);
+        }
+    }
+
+    static void main() {
+        Scanner stdIn = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("요소의 수: ");
+        int num = stdIn.nextInt();
+
+        int[] x = new int[num];
+
+        for (int i = 0; i < x.length; i++) {
+            x[i] = rand.nextInt(100);
+        }
+
+        System.out.println("기존 배열: " + Arrays.toString(x));
+        reverse(x);
+        System.out.println("역순정렬 후 배열: " + Arrays.toString(x));
+    }
+}
